f392f107a30961c41e7e60452bd4632dd27471a2
Lapma <lapostyanm@kkszki.hu>
2025-11-10 08:50:51 +0100

    Basic steps are done.

--- obj/Debug/net8.0/Blog.AssemblyInfo.cs
+++ obj/Debug/net8.0/Blog.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Blog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+411bb52d57884fd4dd367675b27f5f86202d65ee")]
 [assembly: System.Reflection.AssemblyProductAttribute("Blog")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Blog")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
